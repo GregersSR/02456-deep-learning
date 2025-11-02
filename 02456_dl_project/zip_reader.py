ccd3c0d7d3b4880aceca850d7006b61c3a62b544
Gregers Rørdam <gregers@rordam.dk>
2025-11-02 14:36:26 +0100

    Downsample + save to Parquet

--- 02456_dl_project/zip_reader.py
+++ 02456_dl_project/zip_reader.py
@@ -26,15 +26,3 @@ def read_all(dir: Path) -> Generator[pd.DataFrame]:
     zips = sorted(filter(lambda p: p.suffix == '.zip', dir.iterdir()), key=lambda p: p.name)
     for day_f in zips:
         yield read_zip(day_f)
-
-
-def main():
-    zips = sorted(filter(lambda p: p.suffix == '.zip', DATA_DIR.iterdir()), key=lambda p: p.name)
-    for day_f in zips:
-        df = read_zip(day_f)
-
-        for entry in filter(lambda entry: entry[10] == '8317954', read_zip(day_f)):
-            print("\t".join(entry))
-
-if __name__ == '__main__':
-    main()
