a7e36e8d2a70b692cc60e00ee5f1fa65247ff84e
Andreas Espensen Troelsen <andreaset@Andreass-MacBook-Air.local>
2025-11-07 14:05:52 +0100

    added data splitting functionality

--- 02456_dl_project/paths.py
+++ 02456_dl_project/paths.py
@@ -4,7 +4,9 @@ HERE = Path(__file__).parent
 ROOT = HERE.parent
 RAW_DATA_DIR = ROOT / 'data'
 PROCESSED_DATA_DIR = ROOT / 'processed_data'
+SPLITS_DIR = ROOT / 'data_splits'
 
 assert ROOT.is_dir()
 RAW_DATA_DIR.mkdir(exist_ok=True)
 PROCESSED_DATA_DIR.mkdir(exist_ok=True)
+SPLITS_DIR.mkdir(exist_ok=True)
