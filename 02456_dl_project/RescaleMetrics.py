214a5b5f943a5ffa0e23f6673a5c08e2c959645d
pa-ga <gattnerpaula@gmail.com>
2025-12-05 19:48:45 +0100

    Add files via upload

--- 02456_dl_project/RescaleMetrics.py
+++ 02456_dl_project/RescaleMetrics.py
@@ -1,6 +1,7 @@
 import json
 import pandas as pd 
 from pathlib import Path
+import math
 
 
 
@@ -14,8 +15,30 @@ def scale_json_history_and_save(old_path, new_path, divide_by=20):
 
     scaled_history = {}
     for key, values in data["history"].items():
+
         if isinstance(values, list) and all(isinstance(x, (int, float)) for x in values):
-            scaled_history[key] = [x / divide_by for x in values]
+
+            key_lower = key.lower()
+
+            # RMSE: divide by sqrt(20)
+            if "rmse" in key_lower:
+                scaled_history[key] = [x / math.sqrt(divide_by) for x in values]
+
+            # MSE: divide by 20
+            elif "mse" in key_lower:
+                scaled_history[key] = [x / divide_by for x in values]
+
+            # LOSS: divide by 20
+            elif "loss" in key_lower:
+                scaled_history[key] = [x / divide_by for x in values]
+
+            # MAE: divide by 20
+            elif "mae" in key_lower:
+                scaled_history[key] = [x / divide_by for x in values]
+
+            else:
+                scaled_history[key] = values # keep rest untouched if there's something else in history
+
         else:
             scaled_history[key] = values
 
@@ -26,13 +49,14 @@ def scale_json_history_and_save(old_path, new_path, divide_by=20):
     with open(new_path, "w") as f:
         json.dump(data, f, indent=2)
 
-    print(f"Saved scaled file in {new_path.name}")
+    print(f"Saved scaled file → {new_path.name}")
 
 
-MODELS_TXT = Path('models.txt')
 
-OLD_DIR = Path('results_filtered')
-NEW_DIR = Path('results_filtered/results_filtered/newscaled')
+MODELS_TXT = Path('/Users/paulagattner/Downloads/02456-deep-learning-main-3/models.txt')
+
+OLD_DIR = Path('/Users/paulagattner/Downloads/02456-deep-learning-main-3/02456_dl_project/results_filtered/results_filtered')
+NEW_DIR = Path('/Users/paulagattner/Downloads/02456-deep-learning-main-3/02456_dl_project/results_filtered/results_filtered/newscaled')
 
 NEW_DIR.mkdir(parents=True, exist_ok=True)
 
@@ -51,4 +75,9 @@ for model_name in model_names:
 
     new_json = NEW_DIR / old_json.name
 
-    scale_json_history_and_save(old_json, new_json)
+    scale_json_history_and_save(old_json, new_json, divide_by=20)
+
+
+
+
+
